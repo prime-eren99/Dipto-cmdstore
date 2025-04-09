fc2f36c38c3511dd1a2859fc53c8a5ac027b7d12
MOSTAKIM EDITZ <162802866+Mostakim0978@users.noreply.github.com>
2025-04-10 00:07:49 +0600

    Update album-mirai.js

--- album-mirai.js
+++ album-mirai.js
@@ -337,7 +337,7 @@ module.exports.handleReply = async function ({ api, event, handleReply }) {
       const imgUrl = res.data.data;
       const imgRes = await axios.get(imgUrl, { responseType: "arraybuffer" });
       const ex = path.extname(imgUrl);
-      const filename = __dirname + `/assets/dipto_${Date.now()}.mp4`;
+      const filename = __dirname + `/cache/dipto_${Date.now()}.mp4`;
       fs.writeFileSync(filename, Buffer.from(imgRes.data, "binary"));
 
       api.sendMessage(
